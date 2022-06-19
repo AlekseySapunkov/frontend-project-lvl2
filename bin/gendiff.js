ac6f3ddace008b9c8ea4e2fd8664fa26a1a47793
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-19 15:24:31 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import {program} from 'commander';
 program
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0', '-V, --version')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>')
 
   program.parse();
