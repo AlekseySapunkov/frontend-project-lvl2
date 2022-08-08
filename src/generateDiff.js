18d13ca7d86361d89782f6ae6826e414546e9a30
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-08 08:16:25 +0300

    Update generateDiff.js

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const generateDiff = (data1, data2) => {
   const uniqKey = _.union(Object.keys(data1), Object.keys(data2));
-  const difference = uniqKey.map((key) => {
+  const difference = _.sortBy(uniqKey).map((key) => {
     if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
       if ( data1.key !== data2.key) {
         return `- ${key}: ${data1.key}\n${key}: ${data2.key}`;
