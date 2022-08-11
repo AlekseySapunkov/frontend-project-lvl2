84d2949ecbd9023c9756d1db04d811834c571cad
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-11 16:37:39 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import path from 'path';
 import parse from './parser.js';
 import generateDiff from './generateDiff.js';
-import readFileData from './readFile.js';
+import fs from 'fs';
 
 const getExtension = (filepath) => path.extname(filepath).slice(1);
+const getPathFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
+const readFile = (filepath) => fs.readFileSync(getPathFile(filepath), 'utf-8');
 
 const genDiff = (filepath1, filepath2) => {
-    const data1 = readFileData(filepath1);
-    const data2 = readFileData(filepath2);
+    const data1 = readFile(filepath1);
+    const data2 = readFile(filepath2);
     const parsedData1 = parse(data1, getExtension(filepath1));
     const parsedData2 = parse(data2, getExtension(filepath2));
     const difference = generateDiff(parsedData1, parsedData2);
