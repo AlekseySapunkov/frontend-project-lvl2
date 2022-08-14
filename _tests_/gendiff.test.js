cd8ca3966bc1f4d1d918958079198cd884cf0420
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-14 13:40:46 +0300

    Update gendiff.test.js

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,13 +1,15 @@
-import { genDiff } from '../src/index.js';
 import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { resolve } from 'path';
+import genDiff from '../src/index.js';
 
-test('genDiff', () => {
-  expect(genDiff(__fixtures__/file1.json, __fixtures__/file2.json)).toEqual({
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+test('genDiffFlatFile', () => {
+  const dif = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
+  const expectedResult = readFile('flatFileOutput.txt');
+  expect(dif).toEqual(expectedResult);
 });
