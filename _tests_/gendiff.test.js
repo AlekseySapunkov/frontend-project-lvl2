1ec8c7c68e594aff48d3253f17801001dc23fd69
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-16 13:06:55 +0300

    Summary

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -8,8 +8,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
-test('genDiffFlatFile', () => {
-  const dif = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
-  const expectedResult = readFile('flatFileOutput.txt');
+test.each([
+  { a: '__fixtures__/file1.json', b: '__fixtures__/file2.json', c: 'stylish', expected: 'flatFileOutput.txt' },
+  { a: '__fixtures__/file1Recurse.json', b: '__fixtures__/file2Recurse.yaml', c: 'json', expected: 'jsonOutput.txt' },
+  { a: '__fixtures__/file1Recurse.json', b: '__fixtures__/file2Recurse.yaml', c: 'plain', expected: 'plainOutput.txt' },
+  { a: '__fixtures__/file1Recurse.json', b: '__fixtures__/file2Recurse.json', c: 'stylish', expected: 'stylishOutput.txt' }])('genDiffTest', ({a, b, c, expected}) => {
+  const dif = genDiff(a, b, c);
+  const expectedResult = readFile(expected);
   expect(dif).toEqual(expectedResult);
 });
