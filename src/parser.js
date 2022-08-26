58246013419a36b98b26d7c858517b32e40f31cc
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-26 17:30:20 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parse = (data, filetype) => {
-  switch (filetype) {
+const parse = (data, parser) => {
+  switch (parser) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
     case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`Unknown file type: ${filetype}!`);
+      throw new Error(`Unknown parser: ${parser}!`);
   }
 };
 
