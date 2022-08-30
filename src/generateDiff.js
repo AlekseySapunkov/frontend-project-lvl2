eaad8aa31573c2d7f4eeaeec6dcf8a10676842c1
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-30 17:22:39 +0300

    Update generateDiff.js

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -1,25 +1,25 @@
 import _ from 'lodash';
 
-const generateDiff = (tree1, tree2) => {
-  const keys = _.sortBy(_.union(_.keys(tree1), _.keys(tree2)));
+const generateDiff = (data1, data2) => {
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   const difference = keys.map((key) => {
-    if (!Object.hasOwn(tree2, key)) {
-      return { name: key, value: tree1[key], status: 'removed' };
+    if (!Object.hasOwn(data2, key)) {
+      return { name: key, value: data1[key], status: 'removed' };
     }
-    if (!Object.hasOwn(tree1, key)) {
-      return { name: key, value: tree2[key], status: 'added' };
+    if (!Object.hasOwn(data1, key)) {
+      return { name: key, value: data2[key], status: 'added' };
     }
-    if (_.isPlainObject(tree1[key]) && _.isPlainObject(tree2[key])) {
-      return { name: key, status: 'nested', children: generateDiff(tree1[key], tree2[key]) };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { name: key, status: 'nested', children: generateDiff(data1[key],data2[key]) };
     }
-    if (!_.isEqual(tree1[key], tree2[key])) {
+    if (!_.isEqual(data1[key], data2[key])) {
       return {
-        name: key, value: tree2[key], status: 'changed', removedValue: tree1[key],
+        name: key, value: data2[key], status: 'changed', removedValue: data1[key],
       };
     }
 
-    return { name: key, value: tree1[key], status: 'unchanged' };
+    return { name: key, value: data1[key], status: 'unchanged' };
   }, []);
 
   return difference;
