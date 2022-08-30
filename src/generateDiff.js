0995d521cce94324040a7a07bbda4e242e09c5b7
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-30 17:12:53 +0300

    Update generateDiff.js

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const findDiff = (tree1, tree2) => {
+const generateDiff = (tree1, tree2) => {
   const keys = _.sortBy(_.union(_.keys(tree1), _.keys(tree2)));
 
   const difference = keys.map((key) => {
@@ -11,7 +11,7 @@ const findDiff = (tree1, tree2) => {
       return { name: key, value: tree2[key], status: 'added' };
     }
     if (_.isPlainObject(tree1[key]) && _.isPlainObject(tree2[key])) {
-      return { name: key, status: 'nested', children: findDiff(tree1[key], tree2[key]) };
+      return { name: key, status: 'nested', children: generateDiff(tree1[key], tree2[key]) };
     }
     if (!_.isEqual(tree1[key], tree2[key])) {
       return {
@@ -25,4 +25,4 @@ const findDiff = (tree1, tree2) => {
   return difference;
 };
 
-export default findDiff;
+export default generateDiff;
