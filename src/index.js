5bb1e5fc3842f112b26a75b7f1dadfbf87e3824e
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-08-30 17:12:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import findDiff from './generateDiff.js';
+import generateDiff from './generateDiff.js';
 import parse from './parser.js';
 import formatData from './formaters/index.js';
 
@@ -10,7 +10,7 @@ const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8'
 const getData = (filepath) => parse(readFile(filepath), getFileFormat(filepath));
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const diff = findDiff(getData(filepath1), getData(filepath2));
+  const diff = generateDiff(getData(filepath1), getData(filepath2));
   return formatData(diff, format);
 };
 
